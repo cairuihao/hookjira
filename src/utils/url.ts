88947b1148cfd8c08be12d2000714915340de68a
cairuihao <1649640362@qq.com>
2021-07-28 22:13:01 +0800

    封装获取路由参数函数

---
+++ src/utils/url.ts
@@ -0,0 +1,11 @@
+import { useSearchParams } from "react-router-dom"
+
+export const useUrlQueryParam = (keys: string[]) => {
+    const [searchParams, setSearchParam] = useSearchParams();
+    return [
+        keys.reduce((prev:{}, key: string) => {
+            return {...prev, [key]: searchParams.get(key) || ''}
+        }, {} as { [key in string]: string }),
+        setSearchParam
+    ] as const
+}
